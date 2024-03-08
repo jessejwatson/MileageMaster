dc9d59b7dd8c19c8e6cf109629a49bb57e1f2df6
Jesse Watson <jessewatson04@gmail.com>
2024-03-09 08:46:38 +1100

    Initial Commit

---
+++ MileageMaster/MileageMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MileageMasterApp.swift
+//  MileageMaster
+//
+//  Created by Jesse Watson on 09/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct MileageMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
